bb395b97617ad8c1ff36cb1b4d0aa75ebad7e1d7
Hankzz <873468721@qq.com>
2019-03-27 18:52:01 +0800

    加入了数据库和表的配置。“

---
+++ app/src/main/java/com/example/pureweather/db/City.java
@@ -0,0 +1,42 @@
+package com.example.pureweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class City extends LitePalSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceCode;
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+}
